7e610fb5952dec740830dd42271bd493515adf09
Patrick Kunzmann <patrick.kunzm@gmail.com>
2020-07-28 13:24:41 +0200

    Work on MMseqs interface

--- src/biotite/application/mmseqs/search.py
+++ src/biotite/application/mmseqs/search.py
@@ -6,14 +6,32 @@ __name__ = "biotite.application.mmseqs"
 __author__ = "Patrick Kunzmann"
 __all__ = ["MMseqsSearchApp"]
 
+import string
 from tempfile import NamedTemporaryFile, gettempdir
 from subprocess import SubprocessError
+import numpy as np
 from ..localapp import LocalApp
 from ..application import AppState, requires_state
+from ...sequence.alphabet import LetterAlphabet
 from ...sequence.align.alignment import Alignment
+from ...sequence.align.matrix import SubstitutionMatrix
 from ...sequence.io.fasta.file import FastaFile
 from ...sequence.io.fasta.convert import set_sequence
+from ...sequence.sequence import Sequence
 from ...sequence.seqtypes import NucleotideSequence, ProteinSequence
+
+
+class _MappedSequence(Sequence):
+
+    MAX_ALPH_LENGTH = len(string.ascii_uppercase)
+
+    def __init__(self, alph_length):
+        if alph_length > _MappedSequence.MAX_ALPH_LENGTH:
+            raise ValueError("Alphabet length is too large")
+        self._alphabet = LetterAlphabet(string.ascii_uppercase[:alph_length])
+    
+    def get_alphabet(self):
+        return self._alphabet
 
 
 class MMseqsSearchApp(LocalApp):
@@ -22,13 +40,6 @@ class MMseqsSearchApp(LocalApp):
     
     def __init__(self, query, target, matrix, bin_path="mmseqs"):
         super().__init__(bin_path)
-        #if isinstance(query, (tuple, list)):
-        #    self._queries = list(query)
-        #else:
-        #    self._queries = [query]
-        self._target = target
-        self._query = query
-        self._matrix = matrix
 
         if not matrix.is_symmetric():
             raise ValueError("A symmetric substitution matrix is required")
@@ -43,11 +54,16 @@ class MMseqsSearchApp(LocalApp):
                 "The target sequence's alphabet is incompatible with the "
                 "substitution matrix alphabet"
             )
+        
+        self._query = MMseqsSearchApp._map_sequence(query, self._alph)
+        self._target = MMseqsSearchApp._map_sequence(target, self._alph)
+        self._matrix = MMseqsSearchApp._map_matrix(matrix)
 
         self._query_file = NamedTemporaryFile("w", suffix=".fasta")
         self._target_file = NamedTemporaryFile("w", suffix=".fasta")
         self._out_file = NamedTemporaryFile("r", suffix=".results")
         self._matrix_file = NamedTemporaryFile("w", suffix=".out")
+
 
     def run(self):
         for sequence, file in zip(
@@ -58,19 +74,18 @@ class MMseqsSearchApp(LocalApp):
             set_sequence(in_file, sequence)
             in_file.write(file)
             file.flush()
-        #self._matrix_file.write(str(self._matrix))
-        #self._matrix_file.flush()
-        """
-        with open("matrix_test.out", "w") as f:
-            matrix_str = "\n".join(
-                ["\t".join(line.split())
-                 for line in str(self._matrix).splitlines()]
-            )
-            print(matrix_str)
-            f.write(matrix_str)
-        """
         
-        MATRIX_FILE = "matrix_test.out"
+        matrix_str = "\n".join(
+            ["\t".join(line.split())
+             for line in str(self._matrix).splitlines()]
+        )
+        self._matrix_file.write(matrix_str)
+        self._matrix_file.flush()
+        ###
+        with open("test_matrix.out", "w") as f:
+            f.write(matrix_str)
+        ###
+        
         self.set_arguments(
             [
                 "easy-search",
@@ -80,11 +95,11 @@ class MMseqsSearchApp(LocalApp):
                 gettempdir(),
                 "-s", "10.0",
                 "-k", "5",
-                #"--alph-size", str(len(self._alph)),
-                #"--seed-sub-mat", self._matrix_file.name,
-                #"--sub-mat", self._matrix_file.name,
-                "--seed-sub-mat", MATRIX_FILE,
-                "--sub-mat", MATRIX_FILE,
+                "--alph-size", str(len(self._alph)),
+                "--seed-sub-mat", self._matrix_file.name,
+                "--sub-mat", self._matrix_file.name,
+                #"--seed-sub-mat", MATRIX_FILE,
+                #"--sub-mat", MATRIX_FILE,
                 "--dbtype", "1",
                 "--search-type", "1",
                 "--format-output", ",".join([
@@ -109,6 +124,7 @@ class MMseqsSearchApp(LocalApp):
         
         super().run()
     
+
     def evaluate(self):
         super().evaluate()
 
@@ -148,6 +164,7 @@ class MMseqsSearchApp(LocalApp):
             )
             print(alignment)
     
+
     def clean_up(self):
         super().clean_up()
         for file in [
@@ -156,6 +173,36 @@ class MMseqsSearchApp(LocalApp):
         ]:
             file.close()
     
+
     @requires_state(AppState.JOINED)
     def get_alignments(self):
         pass
+
+    
+    @staticmethod
+    def _map_sequence(sequence, alphabet):
+        if len(alphabet) > _MappedSequence.MAX_ALPH_LENGTH:
+            # Cannot map into target alphabet if the alphabet
+            # has more symbols
+            raise TypeError(
+                f"The software cannot align sequences of type "
+                f"{type(sequence).__name__}: "
+                f"Alphabet is too large to be converted into single letters"
+            )
+        # Mapping is done by simply taking over the sequence
+        # code of the original sequence
+        mapped_seq = _MappedSequence(len(alphabet))
+        mapped_seq.code = sequence.code
+        return mapped_seq
+    
+    @staticmethod
+    def _map_matrix(matrix):
+        if not matrix.is_symmetric():
+            raise ValueError("A symmetric matrix is required")
+        # Create a new substitution matrix with the values taken
+        # from the original matrix
+        # Only exchange the alphabets
+        old_alph = matrix.get_alphabet1()
+        new_alph = _MappedSequence(len(old_alph)).get_alphabet()
+        return SubstitutionMatrix(new_alph, new_alph, matrix.score_matrix())
+           
